f038cec76e70c5bd1831163d61f7366a7c4870c9
Mario Foganholi Fernandes <mario.fog.fernandes@protonmail.com>
2025-04-10 11:44:15 -0300

    data models: use array fields in game, drop intermediary classes

--- src/models.py
+++ src/models.py
@@ -3,56 +3,6 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 
-class Game(BaseModel):
-    id: str = Field(default_factory=uuid.uuid4, alias="_id")
-    title: str = Field(...)
-    metacritic_score: int = Field(...)
-    must_play: bool = Field(...)
-    finished: bool = Field(...)
-    fetch_error: bool = Field(...)
-
-    class Config:
-        populate_by_name = True  #allow_population_by_field_name
-        schema_extra = {
-            "example": {
-                "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
-                "title": "Best Game Ever",
-                "metacritic_score": 100,
-                "must_play": True,
-                "finished": False,
-                "fetch_error": False,                
-            }
-        }
-
-
-class Genre(BaseModel):
-    id: str = Field(default_factory=uuid.uuid4, alias="_id")
-    description: str = Field(...)
-
-    class Config:
-        populate_by_name = True
-        schema_extra = {
-            "example": {
-                "_id": "066de609-b04a-4b30-b46c-32537c7f1f6f",
-                "description": "Survival-crafting",
-            }
-        }
-
-
-class GameGenre(BaseModel):
-    game_id: str = Field(...)
-    label_id: str = Field(...)
-
-    class Config:
-        populate_by_name = True
-        schema_extra = {
-            "example": {
-                "game_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
-                "label_id": "066de609-b04a-4b30-b46c-32537c7f1f6f",
-            }
-        }
-
-
 class Label(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
     description: str = Field(...)
@@ -67,15 +17,73 @@ class Label(BaseModel):
         }
 
 
-class GameLabel(BaseModel):
-    game_id: str = Field(...)
-    label_id: str = Field(...)
+class Genre(BaseModel):
+    id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    description: str = Field(...)
 
     class Config:
         populate_by_name = True
         schema_extra = {
             "example": {
-                "game_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
-                "label_id": "066de609-b04a-4b30-b46c-32537c7f1f6g",
+                "_id": "066de609-b04a-4b30-b46c-32537c7f1f6f",
+                "description": "survival-crafting",
             }
         }
+
+
+class Game(BaseModel):
+    id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    title: str = Field(...)
+    metacritic_score: int = Field(...)
+    must_play: bool = Field(...)
+    finished: bool = Field(...)
+    genres: list[Genre] = Field(...)
+    labels: list[Label] = Field(...)
+    fetch_error: bool = Field(...)
+
+    class Config:
+        populate_by_name = True  #allow_population_by_field_name
+        schema_extra = {
+            "example": {
+                "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
+                "title": "Best Game Ever",
+                "metacritic_score": 100,
+                "must_play": True,
+                "finished": False,
+                "genres": [
+                    {"_id": "066de609-b04a-4b30-b46c-32537c7f1f6f", "description": "survival-crafting",}
+                ],
+                "labels": [
+                    {"_id": "066de609-b04a-4b30-b46c-32537c7f1f6g", "description": "my custom collection",}
+                ],
+                "fetch_error": False,            
+            }
+        }
+
+
+# class GameGenre(BaseModel):
+#     game_id: str = Field(...)
+#     label_id: str = Field(...)
+
+#     class Config:
+#         populate_by_name = True
+#         schema_extra = {
+#             "example": {
+#                 "game_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
+#                 "label_id": "066de609-b04a-4b30-b46c-32537c7f1f6f",
+#             }
+#         }
+
+
+# class GameLabel(BaseModel):
+#     game_id: str = Field(...)
+#     label_id: str = Field(...)
+
+#     class Config:
+#         populate_by_name = True
+#         schema_extra = {
+#             "example": {
+#                 "game_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
+#                 "label_id": "066de609-b04a-4b30-b46c-32537c7f1f6g",
+#             }
+#         }
