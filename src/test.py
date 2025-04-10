72db29446b3118a03b3eb7127db533f7d28c0944
Mario Foganholi Fernandes <mario.fog.fernandes@protonmail.com>
2025-04-10 11:44:42 -0300

    add script to test connection to Atlas

---
+++ src/test.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from dotenv import dotenv_values
+from pymongo import MongoClient
+import certifi
+config = dotenv_values(".env")
+app = FastAPI()
+app.mongodb_client = MongoClient(config["ATLAS_URI"], tlsCAFile=certifi.where())
+app.database = app.mongodb_client[config["DB_NAME"]]
+db = app.mongodb_client.get_database(config['DB_NAME'])
+collection = db.get_collection('games')
+collection.count_documents({"title": "Best Game Ever"})
