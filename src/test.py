fe59d5b28b9553609fc5826ae627d13efdbcc80a
Mario Foganholi Fernandes <mario.fog.fernandes@protonmail.com>
2025-05-08 11:13:13 -0300

    final version of the connection-test script

--- src/test.py
+++ src/test.py
@@ -6,6 +6,6 @@ config = dotenv_values(".env")
 app = FastAPI()
 app.mongodb_client = MongoClient(config["ATLAS_URI"], tlsCAFile=certifi.where())
 app.database = app.mongodb_client[config["DB_NAME"]]
-db = app.mongodb_client.get_database(config['DB_NAME'])
-collection = db.get_collection('games')
+collection = app.database.get_collection('games')
 collection.count_documents({"title": "Best Game Ever"})
+# db = app.mongodb_client.get_database(config['DB_NAME'])
