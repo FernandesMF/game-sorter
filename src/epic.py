607934af222b0d316039356f13e0d67754545aa9
Mario Foganholi Fernandes <mario.fog.fernandes@protonmail.com>
2025-05-09 19:16:25 -0300

    implement epic authentication with legendary cli

--- src/epic.py
+++ src/epic.py
@@ -12,12 +12,25 @@ router = APIRouter()
 # """
 
 
+@router.get("")
+async def weblogin_flow():
+
+    is_logged: bool = await check_egs_auth()
+    if not is_logged:
+        return (
+            "Please open the following URL: https://legendary.gl/epiclogin "
+            "and enter the 'authorizationCode' value from the JSON response "
+            "in a POST request to '/epic/authcode'"
+        )
+
+
+# TODO remove endpoint after the whole auth is implemented
 @router.get(
     "/status",
-    response_description="Checks current state of authentication in epiic store",
+    response_description="Checks current state of authentication in epic store",
     response_model=bool,
 )
-async def egs_not_logged() -> bool:
+async def check_egs_auth() -> bool:
 
     msg: str = (
         subprocess.run(["legendary", "status"], capture_output=True)
@@ -32,3 +45,17 @@ async def egs_not_logged() -> bool:
     acc: str = match_.groups()[0]
 
     return acc != "<not logged in>"
+
+
+@router.post("/authcode")
+async def authenticate_with_code(auth_code: str):
+
+    msg = subprocess.run(
+        ["legendary", "auth", "--code", auth_code], capture_output=True
+    )
+
+    is_logged = await check_egs_auth()
+    if not is_logged:
+        return f"Authentication did not work, please try again: {msg.stderr.decode()}"
+
+    return "Authentication successful!"
